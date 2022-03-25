70e9e7cfe8c87f37b8d07aade21d2191a3e543c3
burhandarugar <59868843+burhandarugar@users.noreply.github.com>
2022-03-25 23:18:57 +0530

    Update ALU.v

--- 4_STAGE_PIPELINE/ALU.v
+++ 4_STAGE_PIPELINE/ALU.v
@@ -1,1 +1,20 @@
-
+module ALU(A,B,S,Z,clk);
+parameter N=16;
+input [N-1:0]A,B;
+input [1:0]S;
+input clk;
+output [N-1:0]Z;
+reg [N-1:0]z1;
+ assign Z=z1;
+always @(posedge clk or S)
+  begin
+    if (S==0)
+      z1=A + B ;
+    else if (S==1)
+      z1=A - B ;     
+    else if (S==2)
+      z1=A * B ;
+    else 
+      z1=A ;
+  end
+endmodule
